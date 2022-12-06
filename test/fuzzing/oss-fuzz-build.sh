819729a5ac901fc88f846621cd9d5cb410746551
AdamKorcz <adam@adalogics.com>
2022-12-06 15:18:09 +0000

    fuzzing: Run latest fuzzers in OSS-Fuzz
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- test/fuzzing/oss-fuzz-build.sh
+++ test/fuzzing/oss-fuzz-build.sh
@@ -1,3 +1,13 @@
 #/bin/bash -eu
+printf "package policy\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/cilium/pkg/policy/registerfuzzdep.go
+go mod tidy && go mod vendor
+mv $SRC/cilium/pkg/policy/l4_filter_test.go $SRC/cilium/pkg/policy/l4_filter_test_fuzz.go
+mv $SRC/cilium/pkg/policy/policy_test.go $SRC/cilium/pkg/policy/policy_test_fuzz.go
+mv $SRC/cilium/pkg/policy/rule_test.go $SRC/cilium/pkg/policy/rule_test_fuzz.go
+mv $SRC/cilium/pkg/policy/selectorcache_test.go $SRC/cilium/pkg/pkg/policy/selectorcache_test_fuzz.go
 
 compile_go_fuzzer github.com/cilium/cilium/test/fuzzing Fuzz fuzz gofuzz
+compile_native_go_fuzzer github.com/cilium/cilium/pkg/monitor/format FuzzFormatEvent FuzzFormatEvent
+compile_native_go_fuzzer github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2 FuzzCiliumNetworkPolicyParse FuzzCiliumNetworkPolicyParse
+compile_native_go_fuzzer github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2 FuzzCiliumClusterwideNetworkPolicyParse FuzzCiliumClusterwideNetworkPolicyParse
+compile_native_go_fuzzer github.com/cilium/cilium/pkg/policy FuzzTest FuzzResolveEgressPolicy
